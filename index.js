32ae1d50bc3e5f2850398f7548770951fb28122b
Chan Pham <iamchanpham@gmail.com>
2025-01-31 14:17:35 +0000

    adding images

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const inventory = [
       {
         name: "Classic T-Shirt",
         cost: 19.99,
-        imageUrl: "https://m.media-amazon.com/images/I/51Rm1WGh98L._AC_UY1000_.jpg",
+        imageUrls: ["https://www.trueclassictees.com/cdn/shop/files/4000_BLACK_2_cea5a0d2-2928-432b-b8bf-28a5c88882aa_800x.jpg", "https://www.trueclassictees.com/cdn/shop/files/4000_CARBON_2_6792bbe7-7dc1-47b6-8f43-9b7484909d71_800x.jpg", "https://www.trueclassictees.com/cdn/shop/files/4000_WHITE_2_1a4b3f1d-1093-4b35-bbfd-bf413d2d12be_800x.jpg"],
         path: "/item/0",
         quantity: 120,
         colors: ["black", "white", "gray"]
@@ -17,7 +17,7 @@ const inventory = [
       {
         name: "Denim Jacket",
         cost: 49.99,
-        imageUrl: "https://thursdayboots.com/cdn/shop/files/1024x1024-Mens-Jackets-SelvedgeDenimTrucker-Vintage-091423-1_1024x1024.jpg?v=1695056578",
+        imageUrls: ["https://slimages.macysassets.com/is/image/MCY/products/7/optimized/10138177_fpx.tif?op_sharpen=1&wid=500&fit=fit,1&fmt=webp", "https://slimages.macysassets.com/is/image/MCY/products/8/optimized/10142478_fpx.tif?op_sharpen=1&wid=500&fit=fit,1&fmt=webp", "https://slimages.macysassets.com/is/image/MCY/products/7/optimized/22666017_fpx.tif?op_sharpen=1&wid=500&fit=fit,1&fmt=webp"],
         path: "/item/1",
         quantity: 75,
         colors: ["blue", "black"]
@@ -25,7 +25,7 @@ const inventory = [
       {
         name: "Running Shoes",
         cost: 89.99,
-        imageUrl: "https://i.ebayimg.com/images/g/knUAAOSwA-RjyIAG/s-l1200.jpg",
+        imageUrls: ["https://dks.scene7.com/is/image/GolfGalaxy/22FHQMBND8BLCKWHTMNS_Outerspace?wid=1025&qlt=70&fmt=webp&op_sharpen=1", "https://dks.scene7.com/is/image/GolfGalaxy/22FHQMBND8BLCKWHTMNS_Alabaster?wid=1025&qlt=70&fmt=webp&op_sharpen=1", "https://dks.scene7.com/is/image/GolfGalaxy/22FHQMBND8BLCKWHTMNS_Sharkskin_Grey?wid=1025&qlt=70&fmt=webp&op_sharpen=1"],
         path: "/item/2",
         quantity: 50,
         colors: ["red", "black", "white"]
